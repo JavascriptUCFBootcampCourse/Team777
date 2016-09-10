d41dfd3ec5590e05c81ca26a3fc35bcfe323a649
creativelaurels <lauren@creativelaurels.com>
2016-09-10 11:09:58 -0400

    Added index.html and restaurant.js

---
+++ restaurant.js
@@ -0,0 +1,22 @@
+// Today you will be building a Node / Express based web applications for handling reservation requests.
+
+// Your application will be made up of two parts:
+
+// 1) A front-end set of HTML/CSS/JS pages for entering and viewing data and
+
+// 2) A back-end composed of Node/Express and basic JS for storing, updating, and relaying reservation data.
+
+
+
+
+
+
+
+
+
+// Bonus:
+// If by some miracle you finish early, feel encouraged to work on any one of these additional tasks to take your application to the next level.
+// Add code to your server so that it "counts" every time a person visits any of the pages. Display a running count on the website.
+// Add buttons for "checking off" individuals from the reservation list. Once this happens, the next person on the waitlist should be addd to the main reservation list.
+// Add a button for sending emails to individuals on the wait-list letting them know they have a table ready. You will need to use node-mailer or a similar npm package to make this work. (A hard task, but very cool).
+// Add a button for sending text messages to individuals on the wait-list letting them know they have a table ready. You will need to use the Twilio library to make this work. (A hard task, but very cool).
